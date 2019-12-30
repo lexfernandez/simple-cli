5bbd61bae61f3e8cc7d43bc8de55c82a4ccc1562
lexfernandez <olorenzo@outlook.com>
2019-12-29 22:08:26 -0600

    add first CLI command

--- cli/Program.cs
+++ cli/Program.cs
@@ -12,7 +12,7 @@ namespace cli
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             var config = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
@@ -26,6 +26,35 @@ namespace cli
 
             var people = new List<Person>();
             config.GetSection("people").Bind(people);
+
+            var rootCommand = new RootCommand("My app cli")
+            {
+                new Option("--int-opt","get an int option")
+                {
+                    Argument = new Argument<int>(),
+                    Required = true
+                },
+                new Option("--bool-opt","get a bool option")
+                {
+                    Argument = new Argument<bool>()
+                },
+                new Option("--file-opt","get a file option")
+                {
+                    Argument = new Argument<FileInfo>().ExistingOnly()
+                },
+                
+            };
+            
+            rootCommand.Handler = CommandHandler.Create<int, bool, FileInfo>((intOpt, boolOpt, fileOpt) =>
+            {
+                Console.WriteLine($"an int: {intOpt}");
+                Console.WriteLine($"a bool: {boolOpt}");
+                Console.WriteLine($"a file: {fileOpt}");
+            });
+
+            return rootCommand.Invoke(args);
+
+
         }
     }
 }
